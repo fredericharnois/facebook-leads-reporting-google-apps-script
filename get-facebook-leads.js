c3ed8bea6e09da972635d7b287c6282d0287b5ff
Frederic Harnois <fredericharnois@users.noreply.github.com>
2020-06-22 20:23:40 -0400

    Updated lead retrieval to new API endpoint

--- get-facebook-leads.js
+++ get-facebook-leads.js
@@ -3,7 +3,7 @@
 *
 * Export Facebook Leads to Google Sheets
 *
-* Version: 0.1.1
+* Version: 0.2
 *
 * Google Apps Script maintained by Frederic Harnois
 * fred@fredericharnois.com
@@ -13,32 +13,29 @@
 // MODIFY YOUR SETTINGS HERE //
 
 // url of the google sheets where the report will be
-var SPREADSHEET_URL = 'INSERT_URL'
+const SPREADSHEET_URL = 'INSERT_URL'
 
 // name of the sheet where the report will be
-var TAB_NAME = 'INSERT_TAB_NAME'
+const TAB_NAME = 'INSERT_TAB_NAME'
 
 // user access token linked to a Facebook app
-var TOKEN = 'INSERT_TOKEN'
+const TOKEN = 'INSERT_TOKEN'
 
 // lead form ID
-var FORM_ID = 'INSERT_FORM_ID'
+const FORM_ID = 'INSERT_FORM_ID'
 
 // DO NOT MODIFY ANYTHING BELOW //
 
 function getFacebookLeads() {
 
-  var ss = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
-  var sheet = ss.getSheetByName(TAB_NAME);
+  const ss = SpreadsheetApp.openByUrl(SPREADSHEET_URL);
+  const sheet = ss.getSheetByName(TAB_NAME);
 
   sheet.clear();
   
-  var facebookUrl = 
-    'https://graph.facebook.com/v5.0/' + FORM_ID +
-    '&access_token=' + TOKEN;
-  var response = UrlFetchApp.fetch(facebookUrl);
-  var url = JSON.parse(response).leadgen_export_csv_url;
-  var fetchRequest = UrlFetchApp.fetch(url);
-  var results = Utilities.parseCsv(fetchRequest, '\t');
+  const url = 
+    `https://www.facebook.com/ads/lead_gen/export_csv/?id=${FORM_ID}&type=form&access_token=${TOKEN}`;
+  const fetchRequest = UrlFetchApp.fetch(url);
+  const results = Utilities.parseCsv(fetchRequest, '\t');
   sheet.getRange(1,1, results.length, results[0].length).setValues(results);
 }
